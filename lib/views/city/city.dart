4fc3b59477eaa502724ea0716a4a0ba3dfb38676
Jérémy Lorette <jeremy.lorette@outlook.com>
2024-02-04 23:10:24 +0100

    add trip model and add date picker for selection date of the trip

--- lib/views/city/city.dart
+++ lib/views/city/city.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../data/data.dart' as data;
 import '../../models/activity.model.dart';
+import '../../models/trip.model.dart';
 import 'widgets/activity_card.dart';
 
 class City extends StatefulWidget {
@@ -12,6 +13,23 @@ class City extends StatefulWidget {
 }
 
 class _CityState extends State<City> {
+  Trip mytrip = Trip(activities: [], city: 'Paris', date: DateTime.now());
+
+  void setDate() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now().add(const Duration(days: 1)),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2025),
+    ).then((newDate) {
+      if (newDate != null) {
+        setState(() {
+          mytrip.date = newDate;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,16 +40,39 @@ class _CityState extends State<City> {
           Icon(Icons.more_vert),
         ],
       ),
-      body: Container(
-        padding: const EdgeInsets.all(10),
-        child: GridView.extent(
-          maxCrossAxisExtent: 150,
-          mainAxisSpacing: 2,
-          crossAxisSpacing: 5,
-          children: widget.activities
-              .map((activity) => ActivityCard(activity: activity))
-              .toList(),
-        ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            height: 200,
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    const Expanded(
+                      child: Text('Choisissez une date'),
+                    ),
+                    ElevatedButton(
+                      onPressed: setDate,
+                      child: const Text('Sélectionnez une date'),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 1,
+              crossAxisSpacing: 1,
+              children: widget.activities
+                  .map((activity) => ActivityCard(activity: activity))
+                  .toList(),
+            ),
+          )
+        ],
       ),
     );
   }
