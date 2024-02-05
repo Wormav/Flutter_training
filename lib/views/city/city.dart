7a8a640ff74e046907670015944142a584508cd1
Jérémy Lorette <jeremy.lorette@outlook.com>
2024-02-05 14:45:17 +0100

    add bottom nav

--- lib/views/city/city.dart
+++ lib/views/city/city.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'widgets/trip_activity_list.dart';
+import 'widgets/activity_list.dart';
+import 'widgets/trip_overview.dart';
 import '../../data/data.dart' as data;
 import '../../models/activity.model.dart';
 import '../../models/trip.model.dart';
-import 'widgets/activity_card.dart';
 
 class City extends StatefulWidget {
   final List<Activity> activities = data.activities;
@@ -14,12 +16,13 @@ class City extends StatefulWidget {
 
 class _CityState extends State<City> {
   Trip mytrip = Trip(activities: [], city: 'Paris', date: DateTime.now());
+  int index = 0;
 
   void setDate() {
     showDatePicker(
       context: context,
-      initialDate: DateTime.now().add(const Duration(days: 1)),
       firstDate: DateTime.now(),
+      initialDate: DateTime.now().add(const Duration(days: 1)),
       lastDate: DateTime(2025),
     ).then((newDate) {
       if (newDate != null) {
@@ -30,49 +33,50 @@ class _CityState extends State<City> {
     });
   }
 
+  void switchIndex(newIndex) {
+    setState(() {
+      index = newIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: const Icon(Icons.chevron_left),
-        title: const Text('Paris'),
+        title: const Text('Organisation du voyage'),
         actions: const <Widget>[
           Icon(Icons.more_vert),
         ],
       ),
       body: Column(
         children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            height: 200,
-            color: Colors.white,
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    const Expanded(
-                      child: Text('Choisissez une date'),
-                    ),
-                    ElevatedButton(
-                      onPressed: setDate,
-                      child: const Text('Sélectionnez une date'),
-                    )
-                  ],
-                )
-              ],
-            ),
+          TripOverview(
+            mytrip: mytrip,
+            setDate: setDate,
           ),
           Expanded(
-            child: GridView.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: 1,
-              crossAxisSpacing: 1,
-              children: widget.activities
-                  .map((activity) => ActivityCard(activity: activity))
-                  .toList(),
-            ),
+            child: index == 0
+                ? ActivityList(
+                    activities: widget.activities,
+                  )
+                : const TripActivityList(),
           )
         ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Découverte',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.stars),
+            label: 'Mes activités',
+          )
+        ],
+        onTap: switchIndex,
       ),
     );
   }
---
+++ lib/views/city/widgets/activity_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../../../models/activity.model.dart';
+import 'activity_card.dart';
+
+class ActivityList extends StatelessWidget {
+  final List<Activity> activities;
+
+  const ActivityList({super.key, required this.activities});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 1,
+      crossAxisSpacing: 1,
+      children: activities
+          .map((activity) => ActivityCard(activity: activity))
+          .toList(),
+    );
+  }
+}
